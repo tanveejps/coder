39def5a7393ec5e7c56f9bcff3edecb0dc253c22
tanveejps <tanveejps@gmail.com>
2017-06-05 17:02:28 +0530

    Create oe.java

---
+++ oe.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+class oe
+{
+	public static void main(String args[])
+	{
+		Scanner scan=new Scanner(System.in);
+		int a;
+		a=scan.nextInt();
+		if(a%2==0)
+		{
+			System.out.println("even");
+		}
+		else
+		{
+		System.out.println("odd");
+		}
+	}
+}
