940d0544be4ca31bd80a9233895e695e5c1cddf1
tanveejps <tanveejps@gmail.com>
2017-06-07 16:53:55 +0530

    Create Spiral.java

---
+++ Spiral.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Spiral {
+
+    
+    public static void main(String[] args) {
+    int[][] a={{1,2,3,4},{4,5,6,7},{1,2,3,4},{4,5,6,7}};
+        for(int i=0;i<4;i++)
+        {
+            for(int j=0;j<4;j++)
+            {
+              System.out.print(" "+a[i][j]);
+             }
+            System.out.println(" ");
+            
+        }
+    
+}
